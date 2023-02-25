bf0a54f90d2c2f1ec53ee9ebdaa71c3b661b98fe
aidencj <aidenncj@icloud.com>
2023-02-25 18:49:30 +0800

    Use Promise in get function of IpfsClient

--- modules/IpfsClient.js
+++ modules/IpfsClient.js
@@ -3,63 +3,61 @@ import { readFileSync, writeFileSync, unlink } from 'fs';
 import https from 'https';
 
 export class IpfsClient {
-    constructor(WEB3_STORAGE_TOKEN) {
-        this.client = new Web3Storage({ token: WEB3_STORAGE_TOKEN });
-    }
+  constructor(WEB3_STORAGE_TOKEN) {
+    this.client = new Web3Storage({ token: WEB3_STORAGE_TOKEN });
+  }
 
-    /**
-     * Uploads files to web3.storage
-     * @param {string} path The path to the file.
-     * @return {Promise<CIDString>} Returns the corresponding Content Identifier (CID).
-     */
-    async put(path) {
-        let file = await getFilesFromPath(path)
-        let cid = await this.client.put(file)
-        return cid;
-    }
+  /**
+   * Uploads files to web3.storage
+   * @param {string} path The path to the file.
+   * @return {Promise<CIDString>} Returns the corresponding Content Identifier (CID).
+   */
+  async put(path) {
+    let file = await getFilesFromPath(path)
+    let cid = await this.client.put(file)
+    return cid;
+  }
 
-    /**
-     * Fetch the post object by its CID.
-     * @param {CIDString} cid The Content Identifier (CID) of the post.
-     * @return {object} Returns the post object,
-     * which contains {"author", "title", "context", "emotion"}
-     */
-    async get(cid) {
-        // let res = await this.client.get(cid); // Web3Response
-        // let files = await res.files(); // Web3File[]
-        // let rawData = await files[0].text();
-        let rawData = '';
-        console.log(`CID: ${cid}`);
-        https.get(`https://${cid}.ipfs.w3s.link/Post.json`, (resp) => {
+  /**
+   * Fetch the post object by its CID.
+   * @param {CIDString} cid The Content Identifier (CID) of the post.
+   * @return {object} Returns the post object,
+   * which contains {"author", "title", "context", "emotion"}
+   */
+  get(cid) {
+    return Promise((resolve, reject) => {
+      let rawData = '';
+      console.log(`CID: ${cid}`);
+      https.get(`https://${cid}.ipfs.w3s.link/Post.json`, (resp) => {
 
-            // A chunk of data has been received.
-            resp.on('data', (chunk) => {
-                rawData += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                console.log(rawData);
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+          rawData += chunk;
         });
-        return JSON.parse(rawData);
-    }
 
-    /**
-     * Upload a post to web3.storage
-     * @param {object} postObject The JSON object of the post.
-     * @return {Promise<CIDString>} Returns the corresponding Content Identifier (CID).
-     */
-    async post(postObject){
-        let filename = `Post.json`;
-        writeFileSync(filename, JSON.stringify(postObject));
-        let cid = await this.put(filename);
-        unlink(filename, (err) => {
-            if (err) throw err;
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          resolve(rawData);
         });
-        return cid;
-    }
+
+      }).on("error", (err) => {
+        reject("Error: " + err.message);
+      });
+    })
+  }
+
+  /**
+   * Upload a post to web3.storage
+   * @param {object} postObject The JSON object of the post.
+   * @return {Promise<CIDString>} Returns the corresponding Content Identifier (CID).
+   */
+  async post(postObject){
+    let filename = `Post.json`;
+    writeFileSync(filename, JSON.stringify(postObject));
+    let cid = await this.put(filename);
+    unlink(filename, (err) => {
+      if (err) throw err;
+    });
+    return cid;
+  }
 }
