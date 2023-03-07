012fa0620872faa394105a006a2c17bab40f224f
aidencj <aidenncj@icloud.com>
2023-03-08 02:11:32 +0800

    fix bug at userInfoRouter

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -3,19 +3,19 @@ import {platform} from '../app.js';
 import fs from 'fs';
 export const userInfoRouter = express.Router();
 
-userInfoRouter.post("/api/setUserInfo", async (req, rest) => {
-    console.log(req.body);
-    let image = Buffer.from(req.body.image, 'base64');
-    fs.writeFileSync(req.body.filename, image, 'utf8');
-    let imageCid = platform.ipfsClient.put(req.body.filename);
-    let userInfoObject = {
-        'name': req.body.name,
-        'location': req.body.location,
-        'filename': filename,
-        'imageCid': imageCid
-    };
-    let cid = platform.ipfsClient.uploadUserInfo(userInfoObject);
-    res.send(cid);
-    console.log(`Put ${req.body.name}'s info to IPFS.`);
-    console.log(`CID: ${cid}`);
+userInfoRouter.post("/api/setUserInfo", async (req, res) => {
+  console.log(req.body);
+  let image = Buffer.from(req.body.image, 'base64');
+  fs.writeFileSync(req.body.filename, image, 'utf8');
+  let imageCid = platform.ipfsClient.put(req.body.filename);
+  let userInfoObject = {
+      'name': req.body.name,
+      'location': req.body.location,
+      'filename': req.body.filename,
+      'imageCid': imageCid
+  };
+  let cid = platform.ipfsClient.uploadUserInfo(userInfoObject);
+  res.send(cid);
+  console.log(`Put ${req.body.name}'s info to IPFS.`);
+  console.log(`CID: ${cid}`);
 })
