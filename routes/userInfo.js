9a3e39fbe02a86059f9f3362821ee58909f85426
aidencj <aidenncj@icloud.com>
2023-03-08 02:28:09 +0800

    fix bug: import necessary module

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import {platform} from '../app.js';
-import fs from 'fs';
+import {writeFileSync, unlink} from 'fs';
 export const userInfoRouter = express.Router();
 
 userInfoRouter.post("/api/setUserInfo", async (req, res) => {
   console.log(req.body);
   let image = Buffer.from(req.body.image, 'base64');
-  fs.writeFileSync(req.body.filename, image, 'utf8');
+  writeFileSync(req.body.filename, image, 'utf8');
   let imageCid = await platform.ipfsClient.put(req.body.filename);
   unlink(req.body.filename, (err) => {
     if(err) throw err;
   });
   let userInfoObject = {
-      'name': req.body.name,
-      'location': req.body.location,
-      'filename': req.body.filename,
-      'imageCid': imageCid
+    'name': req.body.name,
+    'location': req.body.location,
+    'filename': req.body.filename,
+    'imageCid': imageCid
   };
   let cid = await platform.ipfsClient.uploadUserInfo(userInfoObject);
   res.send(cid);
